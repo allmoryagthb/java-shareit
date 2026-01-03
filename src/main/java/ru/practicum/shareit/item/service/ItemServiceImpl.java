4eb62f656cc62c631917cad2eecdc5272297abb9
Alexey Morozov <allmorya@yandex.ru>
2026-01-03 18:16:02 +0300

    fix

--- src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.practicum.shareit.exception.EntityNotFoundException;
-import ru.practicum.shareit.exception.ValidationException;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.mapper.ItemMapper;
 import ru.practicum.shareit.item.model.Item;
--- src/main/java/ru/practicum/shareit/user/storage/UserStorage.java
+++ src/main/java/ru/practicum/shareit/user/storage/UserStorage.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.user.storage;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.springframework.stereotype.Repository;
