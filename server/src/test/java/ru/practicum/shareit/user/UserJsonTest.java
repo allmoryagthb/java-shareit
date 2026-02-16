0dacb42dfacdb0b6397ead0102e7cbddb7a23231
Alexey Morozov <allmorya@yandex.ru>
2026-02-16 23:57:54 +0300

    wip

--- server/src/test/java/ru/practicum/shareit/user/UserJsonTest.java
+++ server/src/test/java/ru/practicum/shareit/user/UserJsonTest.java
@@ -13,7 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @JsonTest
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
-public class UserRequestJsonTest {
+public class UserJsonTest {
     private final JacksonTester<UserDto> jsonDto;
 
     @Test
---
+++ server/src/test/java/ru/practicum/shareit/user/UserServiceImplTest.java
@@ -0,0 +1,81 @@
+package ru.practicum.shareit.user;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.service.UserService;
+
+import java.util.Collection;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+@Transactional
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+@TestPropertySource(properties = {"jdbc.url=jdbc:postgresql://localhost:5432/shareit"})
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class UserServiceImplTest {
+    private final EntityManager entityManager;
+    private final UserService userService;
+
+    @Test
+    void saveUserTest() {
+        UserDto userDto = new UserDto(
+                "name", "email@mail.ru"
+        );
+
+        userService.addNewUser(userDto);
+
+        TypedQuery<User> query = entityManager
+                .createQuery("Select u from User u where u.email = :email", User.class);
+        User result = query.setParameter("email", userDto.getEmail())
+                .getSingleResult();
+
+        assertThat(result.getId(), notNullValue());
+        assertThat(result.getId(), equalTo(1L));
+        assertThat(result.getName(), equalTo(userDto.getName()));
+        assertThat(result.getEmail(), equalTo(userDto.getEmail()));
+    }
+
+    @Test
+    void getAllUsersTest() {
+        UserDto userDto1 = new UserDto(
+                "name1", "email1@mail.ru"
+        );
+        UserDto userDto2 = new UserDto(
+                "name2", "email2@mail.ru"
+        );
+        userDto1.setId(userService.addNewUser(userDto1).getId());
+        userDto2.setId(userService.addNewUser(userDto2).getId());
+
+        Collection<UserDto> result = userService.getAllUsersDto();
+
+        assertThat(result.size(), equalTo(2));
+        assertThat(List.copyOf(result).getFirst(), equalTo(userDto1));
+        assertThat(List.copyOf(result).getLast(), equalTo(userDto2));
+    }
+
+    @Test
+    void getUserDtoByIdTest() {
+        UserDto userDto = new UserDto(
+                "name1", "email1@mail.ru"
+        );
+        userService.addNewUser(userDto);
+        UserDto result = userService.getUserDtoById(1L);
+
+        assertThat(result.getId(), notNullValue());
+        assertThat(result.getId(), equalTo(1L));
+        assertThat(result.getName(), equalTo(userDto.getName()));
+        assertThat(result.getEmail(), equalTo(userDto.getEmail()));
+    }
+}
